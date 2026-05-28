0ce2e6d54d8fb2ce774d0c9f950724886a4efc03
iloat20 <iloat20@github.com>
2026-05-28 17:52:34 +0800

    chore: update strategies module exports with backtester classes

--- src/aimoon/strategies/__init__.py
+++ src/aimoon/strategies/__init__.py
@@ -1,5 +1,9 @@
 from aimoon.strategies.base import Strategy
+from aimoon.strategies.backtester import BacktestEngine, BacktestResult, TradeRecord
 from aimoon.strategies.screener import StockScreener, SignalScore
 from aimoon.strategies.technical import TechnicalStrategy
 
-__all__ = ["Strategy", "StockScreener", "SignalScore", "TechnicalStrategy"]
+__all__ = [
+    "Strategy", "StockScreener", "SignalScore", "TechnicalStrategy",
+    "BacktestEngine", "BacktestResult", "TradeRecord",
+]
