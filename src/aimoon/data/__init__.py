f052450780471da2732eca6285961fe7d6693ab5
iloat20 <iloat20@github.com>
2026-06-15 09:00:03 +0800

    refactor: delete dead DataManager, clean data package exports

--- src/aimoon/data/__init__.py
+++ src/aimoon/data/__init__.py
@@ -1,5 +1,6 @@
 """数据获取层"""
 
+from aimoon.cache import DataCache
 from aimoon.data.filters import (
     filter_by_sectors,
     filter_universe,
@@ -7,8 +8,6 @@ from aimoon.data.filters import (
     get_sector_context,
 )
 from aimoon.data.history import get_kline
-from aimoon.cache import DataCache
-from aimoon.data.manager import DataManager, get_manager
 from aimoon.data.spot import get_spot, get_spot_for_codes
 
 __all__ = [
@@ -19,7 +18,5 @@ __all__ = [
     "filter_by_sectors",
     "get_sector_context",
     "get_holdings_pool",
-    "DataManager",
     "DataCache",
-    "get_manager",
 ]
